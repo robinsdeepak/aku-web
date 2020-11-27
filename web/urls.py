d3ef04310677b420fbc2c556c863fccf7bf05a7e
Deepak Kumar <robinsdeepak@gmail.com>
2020-11-27 23:06:04 +0530

    .

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'web'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('results', views.result_links_view, name='result_links'),
 ]
--- web/views.py
+++ web/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def home(request):
     context = {"title": "Home"}
     return render(request, 'base/homepage.html', context)
+
+
+def result_links_view(request):
+    result_links_data = [
+        (batch, {i: "#" for i in range(1, 7)})
+        for batch in ["2016-2020", "2017-2021", "2018-2022", "2019-2023", ]
+    ]
+    context = {
+        "title": "Results",
+        "result_links_data": result_links_data,
+    }
+    return render(request, 'web/result_links.html', context)
